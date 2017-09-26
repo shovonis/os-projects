0c29fd964ec07e195f881849a32c4df7a29e3275
Rifatul Islam <rifatul@rifatul.rifatul>
2017-09-26 14:54:48 -0500

    Fixed Segmentation fault due to pointer issue

--- assign1/source/assign1part3.c
+++ assign1/source/assign1part3.c
@@ -27,33 +27,34 @@ int main(int argc, char *argv[]) {
 
     //Check input params
     if (argc != 5) {
-        fprintf(stderr, "Invalid Params. The prams should be in format:  BytesOfPage SizeOfVirtualMemory SizeOfPhysicalMemory SequenceFile %s \n", argv[0]);
+        fprintf(stderr,
+                "Invalid Params. The prams should be in format:  BytesOfPage SizeOfVirtualMemory SizeOfPhysicalMemory SequenceFile %s \n",
+                argv[0]);
         exit(2);
     }
 
-    unsigned long pages = (unsigned long)atoi(argv[2])/atoi(argv[1]);
-    unsigned long frames = (unsigned long)atoi(argv[3])/atoi(argv[1]);
-    unsigned long pagesize = (unsigned long)ceil(log2(atoi(argv[1])));
+    unsigned long pages = (unsigned long) atoi(argv[2]) / atoi(argv[1]);
+    unsigned long frames = (unsigned long) atoi(argv[3]) / atoi(argv[1]);
+    unsigned long pagesize = (unsigned long) ceil(log2(atoi(argv[1])));
+
+//    printf("Pages: %ld, Frames: %ld, pagesize: %ld \n", pages, frames, pagesize);
 
     //Try to open the output file
-    fileInput = open(argv[1], O_RDONLY);
+    fileInput = open(argv[4], O_RDONLY);
     fileOutput = open("output-part3", O_WRONLY | O_TRUNC | O_CREAT, 0666);
 
     if (fileOutput == -1 || fileInput == -1) {
-        perror("[ERROR] Failed to open the output file");
+        perror("[ERROR] Failed to open the output file ");
     }
 
-    //Initi the values from agrs
+    //Init the values from agrs
     offsetBit = pagesize;
     createPageTable(pages);
     createPhysicalSpace(frames);
 
     int error = 0;
     while (read(fileInput, &virtualAddress, sizeof(unsigned long)) == sizeof(unsigned long)) {
-//        printf("Virtual Address is 0x%lx\n", virtualAddress);
-        offset = virtualAddress & (pow(2, offset) -1); //
-//        printf("offset: %lx\n", offset);
-
+        offset = virtualAddress & (atoi(argv[1]) - 1); //
         virtualAddress = virtualAddress >> offsetBit;
         error = convertPageToFrame(virtualAddress, &frame);
 
